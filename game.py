9ad416084633b2fc35443c666574016ec5d262af
Alejandro <petitmiura@gmail.com>
2023-06-01 22:56:41 +0200

    Ya chocan

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+import math
 
 class Bolas:
     def __init__(self):
@@ -6,45 +8,86 @@ class Bolas:
         self.screen = pygame.display.set_mode((800, 600))
         pygame.display.set_caption("Bolas")
 
-        self.player = Ball(400, 300, 30)
+        self.players = []
+        '''
+        for i in range (random.randint(1, 11)):
+            b = Ball(random.randint(0, 800), random.randint(0, 600), 15,
+                 random.randint(-10, 10), random.randint(-10, 10))
+            self.players.append(b)
+
+        #Variable de player = pelota
+        #self.player = Ball(400, 300, 30, (255, 255, 255), random.randint(-15, 15), random.randint(-10, 10))
+        #self.player = Ball(400, 300, 10, dx=10, dy=15)
+        #self.player2 = Ball(200, 200, 30, (255, 255, 0), random.randint(-15, 15), random.randint(-10, 10))
+        '''
+        self.players.append(
+            Ball(150, 150, 15, 10, 10)
+        )
+        self.players.append(
+            Ball(200, 200, 20, -10, -10)
+        )
         self.metronomo = pygame.time.Clock()
 
     def main_loop(self):
         game_over = False
-        dy = 10 # Delta de y que va a ser 10
-        dx = 10
+
         while game_over == False:
-            self.metronomo.tick(60)
+            self.metronomo.tick(30)
             for evento in pygame.event.get():
                 if evento.type == pygame.QUIT:
                     game_over = True
 
             self.screen.fill ((0, 255, 0))
-            
-            self.player.draw(self.screen)            
-            self.player.x += dx
-            self.player.y += dy
 
-            if self.player.y == 600 - self.player.radio or self.player.y == 0 - self.player.radio:
-                dy = -dy
+            self.players[0].ball_collision(self.players[1])
             
-            if self.player.x == 800 - self.player.radio or self.player.x == 0 - self.player.radio:
-                dx = -dx
+            for bola in self.players:
+                bola.draw(self.screen)
+                bola.update(self.screen)
+                
 
+            
+            #self.player.draw(self.screen)
+            #self.player2.draw(self.screen)  
+            #self.player.update(self.screen)    
+            #self.player2.update(self.screen)        
+      
+            
 
             pygame.display.flip()
 
 class Ball:
-    def __init__(self, x, y, radio, color = (255, 255, 255)):
+    def __init__(self, x, y, radio, dx=0, dy=0):  # ATRIBUTOS (COMPORTAMIENTO, FUNCION Y ESTADO))
         self.x = x
         self.y = y
         self.radio = radio
-        self.color = color
+        self.color = (random.randint(0, 255), random.randint(0, 255),random.randint(0, 255))
+        self.dx = dx
+        self.dy = dy
 
     def draw(self, surface):
         pygame.draw.circle(surface, self.color, (self.x, self.y), self.radio)
         
+    def update(self, surface):
+        self.x += self.dx
+        self.y += self.dy
+
+        if self.y >= surface.get_height() - self.radio or self.y <= self.radio:
+            self.dy = -self.dy
+            
+        if self.x >= surface.get_width() - self.radio or self.x <= self.radio:
+            self.dx = -self.dx
+          
+    def ball_collision(self, otra):
+        distancia = math.sqrt((otra.x - self.x) ** 2 + (otra.y - self.y) ** 2)
+        if distancia <= self.radio + otra.radio:
+            self.dx = - self.dx
+            self.dy = - self.dy
+            otra.dx = - otra.dx
+            otra.dy = - otra.dy
+
 
 if __name__ == "__main__":
-    bola = Bolas()
-    bola.main_loop()
+    juego = Bolas()
+    ball = Ball(400,400,40)
+    juego.main_loop()
